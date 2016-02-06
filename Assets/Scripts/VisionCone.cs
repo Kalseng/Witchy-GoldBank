81d0e6797471c8eedfcf5d1ed0ccd21bdfaa3185
rookjc <rookjc@miamioh.edu>
2016-02-05 19:40:36 -0500

    Added alert system to NPC
    Alert value increments whenever player is within vision cone, based on a
    constant and the player's distance to the NPC. It is also necessary that
    the player be 'suspicious', which is always for now since this will need
    to come from the player script.

--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -2,19 +2,40 @@
 using System.Collections;
 
 public class VisionCone : MonoBehaviour {
+	double alertLevel = 0f;
+	public double ALERT_THRESHOLD;
+	public double ALERT_MOD; // Scale factor for multiplying alert incrementation by
+	bool seesPlayer = false;
+	Transform player;
 
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		bool playerSuspicious = true; // will come from a variable in the player script
+		if (seesPlayer && playerSuspicious) {
+			alertLevel += Time.deltaTime * ALERT_MOD / Mathf.Max((player.position - transform.position).magnitude,1);
+			print (alertLevel);
+			if (alertLevel >= ALERT_THRESHOLD)
+				print ("Suspicion level critical");
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D maybePlayer) {
-		print ("Triggered");
-		print (maybePlayer.transform.name);
+		// print ("Triggered");
+		// print (maybePlayer.transform.name);
+		if (maybePlayer.gameObject.CompareTag ("Player")) {
+			seesPlayer = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D maybePlayer) {
+		if (maybePlayer.gameObject.CompareTag ("Player")) {
+			seesPlayer = false;
+			alertLevel = 0f;
+		}
 	}
 }
