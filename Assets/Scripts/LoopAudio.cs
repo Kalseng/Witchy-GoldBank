3425ef696ce841b84e9bfb3c35ac98120d27ce62
rookjc <rookjc@miamioh.edu>
2016-04-17 20:57:06 -0400

    added main theme track

---
+++ Assets/Scripts/LoopAudio.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopAudio : MonoBehaviour {
+	static double START_TIME = 0.5f;
+	public AudioSource intro;
+	public AudioSource loop;
+
+	// Use this for initialization
+	void Start () {
+		intro.PlayScheduled(AudioSettings.dspTime + START_TIME);
+		loop.PlayScheduled(AudioSettings.dspTime + intro.clip.length + START_TIME);
+	}
+	
+}
