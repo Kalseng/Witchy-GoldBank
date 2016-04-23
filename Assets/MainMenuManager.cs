70dbe46b86e8cc0db358ef7d3d335633069cbd70
Kalseng <mqholm@gmail.com>
2016-04-22 21:26:25 -0400

    menu

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public Text[] menuOptions;
+
+	private int SelectionIndex;
+
+	private float lastSelectTime = -5.0f;
+	private float selectDelay = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+		SelectionIndex = 0;
+		UpdateMenu ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetAxisRaw ("Vertical") != 0 && Time.time - lastSelectTime > selectDelay) {
+			lastSelectTime = Time.time;
+			SelectionIndex -= (int)Input.GetAxisRaw ("Vertical");
+			if (SelectionIndex < 0) {
+				SelectionIndex = menuOptions.Length - 1;
+			} else if (SelectionIndex > menuOptions.Length - 1) {
+				SelectionIndex = 0;
+			}
+			UpdateMenu ();
+		}
+		if (Input.GetAxisRaw ("Vertical") == 0) {
+			lastSelectTime = -5.0f;
+		}
+
+		if (Input.GetButtonDown ("Fire1")) {
+			switch (SelectionIndex) {
+
+			case 0:
+				SceneManager.LoadScene ("Tutorial Scene");
+				break;
+			case 1:
+				Debug.Log ("Credits");
+				break;
+
+			case 2:
+				Application.Quit ();
+				Debug.Log ("Quit");
+				break;
+
+			}
+		}
+
+	}
+
+	void UpdateMenu(){
+		for (int i = 0; i < menuOptions.Length; i++) {
+			menuOptions [i].fontStyle = FontStyle.Normal;
+		}
+		menuOptions [SelectionIndex].fontStyle = FontStyle.Bold;
+	}
+}
